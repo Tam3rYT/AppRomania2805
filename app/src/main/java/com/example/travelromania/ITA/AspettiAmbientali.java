58c64706ddea4ad0fc7551fb66a25e41575ab28c
SGiuri <s.giuri@issm.it>
2021-05-31 16:11:22 +0200

    Versiona Beta 1 funzionante sugli aspetti ambientali

--- app/src/main/java/com/example/travelromania/ITA/AspettiAmbientali.java
+++ app/src/main/java/com/example/travelromania/ITA/AspettiAmbientali.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.travelromania.R;
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class AspettiAmbientali extends AppCompatActivity {
 
@@ -12,5 +13,8 @@ public class AspettiAmbientali extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aspetti_ambientali);
+
+        PDFView pdfView = findViewById(R.id.ambientaliPdf);
+        pdfView.fromAsset("ambientali.pdf").load();
     }
 }
